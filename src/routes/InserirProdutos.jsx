67b13948b25faf5b5ab3e6c982b8421f6bb389a6
Lucasmendoonca <lmendonca775@gmail.com>
2023-09-18 18:54:24 -0300

    criando const navigate e const produtos

--- src/routes/InserirProdutos.jsx
+++ src/routes/InserirProdutos.jsx
@@ -1,3 +1,18 @@
+import { useNavigate} from "react-router-dom";
+
+import React, { useState } from "react";
+
 export default function InserirProdutos() {
+    const navigate = useNavigate();
+
+    const [produto, setProdutos] = useState({
+        id: "",
+        nome: "",
+        desc: "",
+        preco: "",
+    });
+
+    const [produtos, setProduto] = useState([]);
+
     
 }
