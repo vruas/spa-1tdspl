a79850385b18caf00e12aa9037812faaa55f2ec6
vruas <vitor-ruas@live.com>
2023-10-04 20:48:38 -0300

    criando useState e arquivo.scss

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,50 +1,44 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import style from "./ExcluirProdutos.module.css";
+import "./ExcluirProdutos.scss";
+import { useEffect, useState } from "react";
 
 export default function ExcluirProdutos() {
   document.title = "Excluir Produtos";
 
   const navigate = useNavigate();
 
-  //Receber o ID do produto pelo HOOK useParams( );
+  // Receber o ID do produto pelo HOOK useParams( );
   const { id } = useParams();
 
-  //Recuperar o produto na lista pelo ID.
-  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+  const [listaProdutoExterno, setListaProdutoExterno] = useState([]);
+  const [prod, setProd] = useState(null);
 
-  const handleDelete = (event) => {
-    event.preventDefault();
-
-    let indice;
-
-    indice = ListaProdutos.findIndex((item) => item.id === produto.id);
-
-    ListaProdutos.splice(indice, 1);
-
-    alert("Produto exclído com sucesso!");
-
-    navigate("/produtos");
-  };
+ 
 
   return (
     <>
       <div>
         <h1>Excluir Produtos</h1>
-        
-        <div className={style.card}>
+
+        {prod && (
+          <div className={style.card}>
             <h2>Produto Selecionado</h2>
             <figure>
-                <img src={produto.img} alt={produto.desc} title={produto.desc}/>
-                <figcaption>{produto.nome} - <span>R$ </span>{produto.preco}</figcaption>
+              <img src={prod.img} alt={prod.desc} title={prod.desc} />
+              <figcaption>
+                {prod.nome} - <span>R$ </span>
+                {prod.preco}
+              </figcaption>
             </figure>
             <div className={style.btn}>
-                <button onClick={handleDelete}>EXCLUIR</button>
-                <button onClick={()=> navigate("/produtos")}>CANCELAR</button>
+              <button >EXCLUIR</button>
+              <button onClick={() => navigate("/produtos")}>CANCELAR</button>
             </div>
-        </div>
-
+          </div>
+        )}
       </div>
     </>
   );
 }
+
