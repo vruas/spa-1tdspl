012b1efd04643d0561fbac857407a5281f0808bf
vruas <vitor-ruas@live.com>
2023-10-04 16:55:02 -0300

    alterando exluir produtos

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -2,49 +2,49 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import style from "./ExcluirProdutos.module.css";
 
-    export default function ExcluirProdutos() {
-    document.title = "Excluir Produtos";
+export default function ExcluirProdutos() {
+  document.title = "Excluir Produtos";
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //Receber o ID do produto pelo HOOK useParams( );
-    const { id } = useParams();
+  //Receber o ID do produto pelo HOOK useParams( );
+  const { id } = useParams();
 
-    //Recuperar o produto na lista pelo ID.
-    const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+  //Recuperar o produto na lista pelo ID.
+  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleDelete = (event) => {
+    event.preventDefault();
 
-        let indice;
+    let indice;
 
-        indice = ListaProdutos.findIndex((item) => item.id === produto.id);
+    indice = ListaProdutos.findIndex((item) => item.id === produto.id);
 
-        ListaProdutos.splice(indice, 1, produto);
+    ListaProdutos.splice(indice, 1);
 
-        alert("Produto alterado com sucesso!");
+    alert("Produto exclído com sucesso!");
 
-        navigate("/produtos");
-    };
+    navigate("/produtos");
+  };
 
-    return (
-        <>
-        <div>
-            <h1>Excluir Produtos</h1>
-            
-            <div className={style.card}>
-                <h2>Produto Selecionado</h2>
-                <figure>
-                    <img src={produto.img} alt={produto.desc} title={produto.desc}/>
-                    <figcaption>{produto.nome} - <span>R$ </span>{produto.preco}</figcaption>
-                </figure>
-                        <div className={style.btn}> 
-                            <button >EXCLUIR</button>
-                            <button >CANCELAR</button>
-                        </div> 
+  return (
+    <>
+      <div>
+        <h1>Excluir Produtos</h1>
+        
+        <div className={style.card}>
+            <h2>Produto Selecionado</h2>
+            <figure>
+                <img src={produto.img} alt={produto.desc} title={produto.desc}/>
+                <figcaption>{produto.nome} - <span>R$ </span>{produto.preco}</figcaption>
+            </figure>
+            <div className={style.btn}>
+                <button onClick={handleDelete}>EXCLUIR</button>
+                <button onClick={()=> navigate("/produtos")}>CANCELAR</button>
             </div>
-
         </div>
-        </>
-    );
-    }
+
+      </div>
+    </>
+  );
+}
