296389a86577272846c54a71098d00f5d54adf8c
Lucasmendoonca <lmendonca775@gmail.com>
2023-10-04 21:10:08 -0300

    adicionando useEffect e o fect com o metodo get

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,50 +1,63 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import style from "./ExcluirProdutos.module.css";
+import "./ExcluirProdutos.scss";
+import { useEffect, useState } from "react";
 
-    export default function ExcluirProdutos() {
-    document.title = "Excluir Produtos";
+export default function ExcluirProdutos() {
+  document.title = "Excluir Produtos";
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //Receber o ID do produto pelo HOOK useParams( );
-    const { id } = useParams();
+  // Receber o ID do produto pelo HOOK useParams( );
+  const { id } = useParams();
 
-    //Recuperar o produto na lista pelo ID.
-    const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+  const [listaProdutoExterno, setListaProdutoExterno] = useState([]);
+  const [prod, setProd] = useState(null);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  useEffect(() => {
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Produto não encontrado");
+        }
+      })
+      .then((data) => {
+        setProd(data);
+      })
+      .catch((error) => console.error(error));
+  }, [id]);
 
-        let indice;
+  
 
-        indice = ListaProdutos.findIndex((item) => item.id === produto.id);
+  return (
+    <>
+      <div>
+        <h1>Excluir Produtos</h1>
 
-        ListaProdutos.splice(indice, 1, produto);
-
-        alert("Produto alterado com sucesso!");
-
-        navigate("/produtos");
-    };
-
-    return (
-        <>
-        <div>
-            <h1>Excluir Produtos</h1>
-            
-            <div className={style.card}>
-                <h2>Produto Selecionado</h2>
-                <figure>
-                    <img src={produto.img} alt={produto.desc} title={produto.desc}/>
-                    <figcaption>{produto.nome} - <span>R$ </span>{produto.preco}</figcaption>
-                </figure>
-                        <div className={style.btn}> 
-                            <button >EXCLUIR</button>
-                            <button >CANCELAR</button>
-                        </div> 
+        {prod && (
+          <div className={style.card}>
+            <h2>Produto Selecionado</h2>
+            <figure>
+              <img src={prod.img} alt={prod.desc} title={prod.desc} />
+              <figcaption>
+                {prod.nome} - <span>R$ </span>
+                {prod.preco}
+              </figcaption>
+            </figure>
+            <div className={style.btn}>
+              <button onClick={handleDelete}>EXCLUIR</button>
+              <button onClick={() => navigate("/produtos")}>CANCELAR</button>
             </div>
-
-        </div>
-        </>
-    );
-    }
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
